014d9b26f97f8753429dd898e509d616fa5e543d
Aryaan Shaikh <57898150+Aryaan03@users.noreply.github.com>
2024-03-11 22:12:53 -0400

    Update censoror.py

--- censoror.py
+++ censoror.py
@@ -12,11 +12,11 @@ from nltk.corpus import stopwords
 from google.cloud import language_v1  
 # Google Cloud Natural Language API
 
-spacy.cli.download("en_core_web_md") # Downloading the English model for spaCy
+# spacy.cli.download("en_core_web_md") # Downloading the English model for spaCy
 
 Model = spacy.load("en_core_web_md") # Loading the English model for spaCy
 
-def parg():
+def ArgParse():
     # Creating an argument parser instance
     varx = argparse.ArgumentParser(description="Censoring important and sensitive data from text documents")
     # Adding arguments to the argument parser
@@ -26,12 +26,75 @@ def parg():
     varx.add_argument('--dates', action='store_true', help='Censoring Dates')
     varx.add_argument('--phones', action='store_true', help='Censoring Phone Numbers')
     varx.add_argument('--address', action='store_true', help='Censoring Location or Address')
-    varx.add_argument('--stats', choices=['stdout', 'stderr'], default='stderr', help='Statistics Output Destination')
+    varx.add_argument('--stats', type=str, help='Statistics Output Destination')
     return varx.parse_args()
 
-def case(x):
-    # Filtering out elements not matching the specified pattern
-    return [temp for temp in x if not re.match(r'^\d{4}$', temp)]
+def CenName(MailData):
+    Mod = Model(MailData)
+    # Extracting person label from the text using spaCy
+    return [ent.text for ent in Mod.ents if ent.label_ == "PERSON"]
+    
+def CenDate(MailData):
+    # Initializing Google Cloud Natural Language API 
+    GoogleMod = language_v1.LanguageServiceClient.from_service_account_json('key.json')
+    file = language_v1.Document(content=MailData, type_=language_v1.Document.Type.PLAIN_TEXT)
+    # Analyzing entities in the text using Google Cloud Natural Language API
+    response = GoogleMod.analyze_entities(document=file, encoding_type=language_v1.EncodingType.UTF8)
+    x = response.entities
+    # Extracting Dates from the text using Google Cloud Natural Language API 
+    return [entity.name for entity in x if language_v1.Entity.Type(entity.type_).name in ["DATE"]]
+
+def CenNum(MailData):
+    # Initializing Google Cloud Natural Language API 
+    GoogleMod = language_v1.LanguageServiceClient.from_service_account_json('key.json')
+    file = language_v1.Document(content=MailData, type_=language_v1.Document.Type.PLAIN_TEXT)
+    # Analyzing entities in the text using Google Cloud Natural Language API
+    response = GoogleMod.analyze_entities(document=file, encoding_type=language_v1.EncodingType.UTF8)
+    x = response.entities
+    # Extracting Phone Numbers from the text using Google Cloud Natural Language API 
+    return [entity.name for entity in x if language_v1.Entity.Type(entity.type_).name in ["PHONE_NUMBER"]]
+
+def CenLoc(MailData):
+    # Initializing Google Cloud Natural Language API 
+    GoogleMod = language_v1.LanguageServiceClient.from_service_account_json('key.json')
+    file = language_v1.Document(content=MailData, type_=language_v1.Document.Type.PLAIN_TEXT)
+    # Analyzing entities in the text using Google Cloud Natural Language API
+    response = GoogleMod.analyze_entities(document=file, encoding_type=language_v1.EncodingType.UTF8)
+    x = response.entities
+    # Extracting Address from the text using Google Cloud Natural Language API 
+    return [entity.name for entity in x if language_v1.Entity.Type(entity.type_).name in ["ADDRESS"]]
+
+def analyze_entities(MailData, FlaTyp):
+    # Analyzing entities in the provided text using spaCy and Google Cloud Natural Language API
+    y=[] # calculating the number of entity flags
+    stat = [] #Calculating stats 
+
+    # Counting occurrences of different types of entity flags 
+    if FlaTyp['names']: 
+            var1= CenName(MailData)
+            y.extend(var1) # Extend the list y with the extracted names
+            stat.append(len(var1)) 
+    else: stat.append(0) # If 'names' flag is not set, append 0 to the stat list
+
+    if FlaTyp['dates']:
+            var2= CenDate(MailData)
+            y.extend(var2) # Extend the list y with the extracted dates
+            stat.append(len(var2))
+    else: stat.append(0) # If 'dates' flag is not set, append 0 to the stat list
+
+    if FlaTyp['phones']:
+            var3= CenNum(MailData)
+            y.extend(var3) # Extend the list y with the extracted phone number
+            stat.append(len(var3))
+    else: stat.append(0) # If 'phones' flag is not set, append 0 to the stat list
+
+    if FlaTyp['address']:
+            var4= CenLoc(MailData)
+            y.extend(var4) # Extend the list y with the extracted addresses 
+            stat.append(len(var4))
+    else: stat.append(0) # If 'address' flag is not set, append 0 to the stat list
+
+    return y, stat
 
 def censor(info, type):
     # Censoring sensitive information in the provided text
@@ -41,59 +104,32 @@ def censor(info, type):
         info = info.replace(x, code)  
     return info
 
-def analyze_entities(MailData):
-    # Analyzing entities in the provided text using spaCy and Google Cloud Natural Language API
-    stat = []
-    Mod = Model(MailData) 
-
-    # Extracting person label from the text using spaCy
-    Person = [ent.text for ent in Mod.ents if ent.label_ == "PERSON"]
-
-    # Initializing Google Cloud Natural Language API client
-    GoogleMod = language_v1.LanguageServiceClient.from_service_account_json('key.json')
-    file = language_v1.Document(content=MailData, type_=language_v1.Document.Type.PLAIN_TEXT)
-    # Analyzing entities in the text using Google Cloud Natural Language API
-    response = GoogleMod.analyze_entities(document=file, encoding_type=language_v1.EncodingType.UTF8)
-
-    x = response.entities
-    # Extracting phone numbers, addresses, and dates from the analyzed entities
-    verify= [entity.name for entity in x if language_v1.Entity.Type(entity.type_).name in ["PHONE_NUMBER"]]
-    verify= [entity.name for entity in x if language_v1.Entity.Type(entity.type_).name in ["ADDRESS"]]
-    verify= [entity.name for entity in x if language_v1.Entity.Type(entity.type_).name in ["DATE"]]
-
-    Phone_Numbers=0
-    Locations=0
-    Dates=0
-
-    # Counting occurrences of different types of entities
-    for entity in x:
-        if language_v1.Entity.Type(entity.type_).name == 'DATE' :
-            Dates += 1
-        elif language_v1.Entity.Type(entity.type_).name == "PHONE_NUMBER" :
-            Phone_Numbers += 1
-        elif language_v1.Entity.Type(entity.type_).name == "ADDRESS" :
-            Locations += 1
-
-    stat = [Dates, Phone_Numbers, Locations, len(Person)]  
-    verify += Person  # Combining detected entities and persons
-    return verify, stat
-
-def CenP(data):
+def CenP(data, FlaTyp):
     # Censoring sensitive information in the provided text
-    entities, count= analyze_entities(data)
-    data = censor(data, entities)
+    x, stat= analyze_entities(data, FlaTyp) # Calling analyze_entites to check flag inputted 
+    data = censor(data, x) # Call censor function to censor sensitive data with Block character 
     # Printing statistics of censored flags
-    print ("Censored Names:",count[3], " Censored Date:", count[1], " Censored Phone Numbers:", count[2], " Censored Addresses:", count[0])
+    NewArg =ArgParse()
+    if NewArg.stats=='stdout' or  NewArg.stats=='stderr': # Printing statistics of censored flags in stderr or stdout
+        print ("Censored Names:",stat[0], " Censored Date:", stat[1], " Censored Phone Numbers:", stat[2], " Censored Addresses:", stat[3])
+    else: 
+        Censored_Statistics=NewArg.stats #Printing statistics of censored flags in .txt files format
+        with open(Censored_Statistics, "a", encoding="utf-8") as x:
+             x.write(f'Censored Names: {stat[0]}   Censored Date: {stat[1]}   Censored Phone Numbers: {stat[2]}   Censored Addresses: {stat[3]}\n')
     return data
 
-def Read(Xtemp, CCd):
+def case(x):
+    # Filtering out elements not matching the specified pattern
+    return [temp for temp in x if not re.match(r"^\d{4}$", temp)]
+
+def Read(Xtemp, CCd, FlaTyp):
     # Reading input files, censoring sensitive information, and writing censored files
     for x in Xtemp:
         try:
-            with open(x, 'r', encoding="utf-8") as file:
+            with open(x, "r", encoding="utf-8") as file:
                 data = file.read()
 
-            censored = CenP(data)  # Censoring sensitive information in the text
+            censored = CenP(data, FlaTyp)  # Censoring sensitive information in the text
             New = os.path.basename(x) + '.censored'
             Odir = os.path.join(CCd, New)
             os.makedirs(CCd, exist_ok=True)
@@ -104,16 +140,16 @@ def Read(Xtemp, CCd):
 
 def main():
     # Main function for handling program execution
-    tem = parg()
+    tem = ArgParse()
     if not tem.input:
-        print('Please give input file')  # Prompting user to provide input files
+        print('Error!! Please give input file')  # Prompting user to provide input files
     else:
         list = []
         for x in tem.input:
             list.extend(glob.glob(os.path.join('./', x)))  # Finding files matching the provided patterns
 
         if not list:
-            print('Specific extension file not found')  # Alerting user if specified files are not found
+            print('Error!! Specific extension file not found')  # Alerting user if specified files are not found
         else:
             os.makedirs(tem.output, exist_ok=True)  # Creating the output directory if it doesn't exist
 
@@ -121,9 +157,9 @@ def main():
             SFlag = [flag for flag, value in temp.items() if value]
 
             if SFlag:
-                Read(list, tem.output)  # Reading and censoring files if censoring flags are specified
+                Read(list, tem.output, temp)  # Reading and censoring files if censoring flags are specified
             else:
-                print("Mention some Censor flag")  # Prompting user to specify censoring flags
+                print("Error!! Mention some Censor flag (no less than 1)!")  # Prompting user to specify censoring flags
 
 if __name__ == "__main__":
     main()  
